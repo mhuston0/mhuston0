18f4bee685c5b9064301131e53bf31f36075a973
Matthew <mhuston0+github@gmail.com>
2021-08-30 13:09:12 -0600

    4.5 Challenge
    Have user enter password and display capitalized first letter.

---
+++ first_letter.py
@@ -0,0 +1,6 @@
+#4.5 Challenge: Have user enter password and display capitalized first letter.
+
+user_input = input("Tell me your password: ")
+first_letter = user_input[0]
+print("The first letter you entered was: " + first_letter.upper())
+
