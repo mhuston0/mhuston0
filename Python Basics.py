66e649d6699940c5e2e315e9f92d2230fca61856
Matthew <mhuston0+github@gmail.com>
2021-08-29 20:43:57 -0600

    4.3 Review exercises
    Exercise 5 make .startswith("be") true for each.

--- Python Basics.py
+++ Python Basics.py
@@ -1,8 +1,15 @@
 string1 = "Becomes"
+string1 = string1.lower()
 print(string1.startswith("be"))
+
 string2 = "becomes"
 print(string2.startswith("be"))
+#string2 unchanged
+
 string3 = "BEAR"
+string3 = string3.lower()
 print(string3.startswith("be"))
+
 string4 = "    bEautiful"
+string4 = string4.lower().lstrip()
 print(string4.startswith("be"))
